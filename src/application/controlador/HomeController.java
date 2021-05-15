5df5591528f4dec52c7cee07e2d1a02e8e9f4e11
Sergio <sergiodurancazorla@gmail.com>
2021-05-15 19:47:42 +0200

    diseño HOME nuevo, se rellena tabla y pantalla home

--- src/application/controlador/HomeController.java
+++ src/application/controlador/HomeController.java
@@ -1,20 +1,34 @@
 package application.controlador;
 
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXTreeTableColumn;
 import com.jfoenix.controls.JFXTreeTableView;
+import com.jfoenix.controls.RecursiveTreeItem;
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 
 import application.modelo.Main;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableColumn.CellDataFeatures;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Callback;
+import orm.dao.DaoIncidencia;
 import orm.pojos.Incidencia;
 import orm.pojos.Profesor;
 import orm.pojos.RolPermiso;
+import utiles.excepciones.BusinessException;
 
 public class HomeController implements Initializable {
 
@@ -58,22 +72,29 @@ public class HomeController implements Initializable {
 	private JFXCheckBox checkInformes;
 
 	// MODIFICAR************************************************************
-	private int totalIncidencias = 5;
+	private int totalIncidencias;
 	private Profesor profesor;
+	private ObservableList<Incidencia> data;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		profesor = Main.profesor;
 
-		iniciarTabla();
+		try {
+			iniciarTabla();
 
-		txtNombreSuperior.setText(profesor.toString());
-		txtNombre.setText(profesor.toString());
-		txtEmail.setText(profesor.getEmail());
-		txtDepartamento.setText(profesor.getDepartamento().getNombre());
-		txtTotalIncidencias.setText("Total de incidencias creadas: " + totalIncidencias);
+			txtNombreSuperior.setText(profesor.toString());
+			txtNombre.setText(profesor.toString());
+			txtEmail.setText(profesor.getEmail());
+			txtDepartamento.setText(profesor.getDepartamento().getNombre());
+			totalIncidencias = tablaIncidencias.getExpandedItemCount();
+			txtTotalIncidencias.setText("Total de incidencias creadas: " + totalIncidencias);
 
-		rellenarChecks();
+			rellenarChecks();
+
+		} catch (BusinessException e) {
+			e.printStackTrace();
+		}
 
 	}
 
@@ -121,9 +142,150 @@ public class HomeController implements Initializable {
 
 	}
 
-	private void iniciarTabla() {
-		// TODO Auto-generated method stub
+	@SuppressWarnings("unchecked")
+	private void iniciarTabla() throws BusinessException {
+		// DAO NECESARIO
+		orm.dao.DaoIncidencia daoIncidencia = new DaoIncidencia();
 
+		// Lista incidencias por profesor
+		data = FXCollections.observableArrayList(daoIncidencia.getIncidenciasProfesor(profesor));
+		// -------COLUMNAS-----------
+
+		// Estado
+		JFXTreeTableColumn<Incidencia, String> columnaId = new JFXTreeTableColumn<>("Estado");
+		columnaId.setCellValueFactory(
+				new Callback<TreeTableColumn.CellDataFeatures<Incidencia, String>, ObservableValue<String>>() {
+
+					@Override
+					public ObservableValue<String> call(CellDataFeatures<Incidencia, String> param) {
+						return new SimpleStringProperty(param.getValue().getValue().getEstado().getNombre());
+					}
+				});
+		columnaId.setContextMenu(null);
+
+		// TIPO
+		JFXTreeTableColumn<Incidencia, String> columnaTipo = new JFXTreeTableColumn<>("Tipo");
+		columnaTipo.setCellValueFactory(
+				new Callback<TreeTableColumn.CellDataFeatures<Incidencia, String>, ObservableValue<String>>() {
+
+					@Override
+					public ObservableValue<String> call(CellDataFeatures<Incidencia, String> param) {
+						return new SimpleStringProperty(param.getValue().getValue().getTipo().getTipo());
+					}
+				});
+		columnaTipo.setContextMenu(null);
+
+		// FECHA
+
+		JFXTreeTableColumn<Incidencia, String> columnaFecha = new JFXTreeTableColumn<>("Fecha creada");
+		columnaFecha.setCellValueFactory(
+				new Callback<TreeTableColumn.CellDataFeatures<Incidencia, String>, ObservableValue<String>>() {
+
+					@Override
+					public ObservableValue<String> call(CellDataFeatures<Incidencia, String> param) {
+
+						SimpleDateFormat format = new SimpleDateFormat("dd-MM-YYYY");
+
+						return new SimpleStringProperty(
+								format.format(param.getValue().getValue().getFechaIncidencia()).toString());
+					}
+				});
+		columnaFecha.setContextMenu(null);
+
+		// PROFESOR
+		JFXTreeTableColumn<Incidencia, String> columnaProfesor = new JFXTreeTableColumn<>("Informador");
+		columnaProfesor.setCellValueFactory(
+				new Callback<TreeTableColumn.CellDataFeatures<Incidencia, String>, ObservableValue<String>>() {
+
+					@Override
+					public ObservableValue<String> call(CellDataFeatures<Incidencia, String> param) {
+
+						return new SimpleStringProperty(
+								param.getValue().getValue().getProfesorByProfesorIdprofesor().toString());
+					}
+				});
+		columnaProfesor.setContextMenu(null);
+
+		// DEPARTAMENTO
+
+		JFXTreeTableColumn<Incidencia, String> columnaDepartamento = new JFXTreeTableColumn<>("Departamento");
+		columnaDepartamento.setCellValueFactory(
+				new Callback<TreeTableColumn.CellDataFeatures<Incidencia, String>, ObservableValue<String>>() {
+
+					@Override
+					public ObservableValue<String> call(CellDataFeatures<Incidencia, String> param) {
+
+						return new SimpleStringProperty(param.getValue().getValue().getDepartamento().getNombre());
+					}
+				});
+		columnaDepartamento.setContextMenu(null);
+
+		// AULA
+
+		JFXTreeTableColumn<Incidencia, String> columnaAula = new JFXTreeTableColumn<>("Aula");
+		columnaAula.setCellValueFactory(
+				new Callback<TreeTableColumn.CellDataFeatures<Incidencia, String>, ObservableValue<String>>() {
+
+					@Override
+					public ObservableValue<String> call(CellDataFeatures<Incidencia, String> param) {
+
+						return new SimpleStringProperty(param.getValue().getValue().getAula().getDescripcion());
+					}
+				});
+		columnaAula.setContextMenu(null);
+
+		// PROFESOR RESOLUCION
+
+		JFXTreeTableColumn<Incidencia, String> columnaResolucionProfesor = new JFXTreeTableColumn<>("Responsable");
+		columnaResolucionProfesor.setCellValueFactory(
+				new Callback<TreeTableColumn.CellDataFeatures<Incidencia, String>, ObservableValue<String>>() {
+
+					@Override
+					public ObservableValue<String> call(CellDataFeatures<Incidencia, String> param) {
+						SimpleStringProperty valor = new SimpleStringProperty();
+						if (param.getValue().getValue().getProfesorByResponsableSolucion() == null) {
+							valor.set("Sin asignar");
+						} else {
+							valor.set(param.getValue().getValue().getProfesorByResponsableSolucion().toString());
+						}
+
+						return valor;
+					}
+				});
+		columnaResolucionProfesor.setContextMenu(null);
+
+		// FECHA RESOLUCION
+
+		JFXTreeTableColumn<Incidencia, String> columnaResolucionFecha = new JFXTreeTableColumn<>("Resolucion");
+		columnaResolucionFecha.setCellValueFactory(
+				new Callback<TreeTableColumn.CellDataFeatures<Incidencia, String>, ObservableValue<String>>() {
+
+					@Override
+					public ObservableValue<String> call(CellDataFeatures<Incidencia, String> param) {
+						SimpleStringProperty valor = new SimpleStringProperty();
+						if (param.getValue().getValue().getFechaResolucion() == null) {
+							valor.set("No resuelta");
+						} else {
+							SimpleDateFormat format = new SimpleDateFormat("dd-MM-YYYY");
+
+							valor.set(format.format(param.getValue().getValue().getFechaResolucion()).toString());
+						}
+
+						return valor;
+					}
+				});
+		columnaResolucionFecha.setContextMenu(null);
+
+		final TreeItem<Incidencia> root = new RecursiveTreeItem<Incidencia>(data, RecursiveTreeObject::getChildren);
+
+		// Add a la tabla las columnas
+
+		tablaIncidencias.getColumns().setAll(columnaId, columnaTipo, columnaFecha, columnaProfesor, columnaDepartamento,
+				columnaAula, columnaResolucionProfesor, columnaResolucionFecha);
+
+		// Caracteristicas de las tablas
+		tablaIncidencias.setRoot(root);
+		tablaIncidencias.setShowRoot(false);
 	}
 
 }
--- src/orm/dao/DaoIncidencia.java
+++ src/orm/dao/DaoIncidencia.java
@@ -1,11 +1,32 @@
 package orm.dao;
 
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
 import orm.pojos.Incidencia;
+import orm.pojos.Profesor;
 import utiles.dao.DaoGenericoHibernate;
+import utiles.hibernate.UtilesHibernate;
 
 public class DaoIncidencia extends DaoGenericoHibernate<Incidencia, Integer> {
 	private final static Logger LOGGER = Logger.getLogger(Incidencia.class.getName());
+
+	@SuppressWarnings("unchecked")
+	public ArrayList<Incidencia> getIncidenciasProfesor(Profesor p) {
+		ArrayList<Incidencia> lista = new ArrayList<Incidencia>();
+		// comenzamos sesion
+		Session s = UtilesHibernate.getSessionFactory().getCurrentSession();
+
+		// Obtener listado incidencia
+		String hql = "SELECT i FROM Incidencia i where profesor_idprofesor=:dni";
+		Query query = s.createQuery(hql);
+		query.setParameter("dni", p.getDni());
+		lista = (ArrayList<Incidencia>) query.list();
+
+		return lista;
+	}
 
 }
